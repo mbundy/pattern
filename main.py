91053a2ce035c6e5d0c9fc5729994416b1e45dc2
mbundy <P1l0tM1k3>
2021-09-10 10:26:14 -0700

    Update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # import urllib library
 # import json
+import atexit
+
 import daemon
 import time
 import json
@@ -7,47 +9,61 @@ from urllib.request import urlopen
 import importlib.util
 
 try:
-    importlib.util.find_spec('RPi.GPIO')
-    import RPi.GPIO as GPIO
+	importlib.util.find_spec('RPi.GPIO')
+	import RPi.GPIO as GPIO
 except ImportError:
-    """
-    import FakeRPi.GPIO as GPIO
-    OR
-    import FakeRPi.RPiO as RPiO
-    """
+	import FakeRPi.GPIO as GPIO
 
-    import FakeRPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setmode(GPIO.OUT)
 
 # store the URL in url as
 # parameter for urlopen
-url = "https://datis.clowd.io/api/kvny"
 
-# store the response of URL
-response = urlopen(url)
 
-# storing the JSON response 
-# from url in data
-data_json = json.loads(response.read())
+def reversepattern():
+	# store the response of URL
+	url = "https://datis.clowd.io/api/kvny"
+	response = urlopen(url)
 
-# print the json response
-print(data_json)
+	# storing the JSON response
+	# from url in data
+	data_json = json.loads(response.read())
 
-airport = data_json[0]
-datis = airport['datis']
-#print(datis)
-runwaystart: int = datis.find('LNDG AND DEPG')
-#print(datis[runwaystart:])
-runwayend: int = datis[runwaystart:].find('.')
-landing = datis[runwaystart:runwaystart + runwayend]
-if landing != -1:
-    if landing.find('34') > 0:
-        print("Reverse pattern...")
-        GPIO.output(18, GPIO.HIGH)
-    else:
-        print("Normal pattern...")
-        GPIO.output(18, GPIO.LOW)
+	# print the json response
+	print(data_json)
 
-GPIO.cleanup(18)
+	airport = data_json[0]
+	datis = airport['datis']
+	# print(datis)
+	runwaystart: int = datis.find('LNDG AND DEPG')
+	if runwaystart != -1:
+		runwayend: int = datis[runwaystart:].find('.')
+		landing = datis[runwaystart:runwaystart + runwayend]
+		if landing.find('34') > 0:
+			return True
+		else:
+			return False
+	else:
+		return False
+
+
+def run():
+	while True:
+		if reversepattern():
+			print("Reverse pattern...")
+			GPIO.output(18, GPIO.HIGH)
+		else:
+			print("Normal pattern...")
+			GPIO.output(18, GPIO.LOW)
+		time.sleep(5)
+
+
+def cleanup():
+	GPIO.cleanup(18)
+
+
+if __name__ == "__main__":
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.OUT)
+	atexit.register(cleanup)
+	run()
+
