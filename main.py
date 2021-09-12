ea4393bded0b6b97bbcec42fd5798058a90ff96d
mbundy <P1l0tM1k3>
2021-09-12 10:59:24 -0700

    Update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import sys
 import signal
 import atexit
+from urllib.error import URLError
+
 import daemon
 import time
 import json
@@ -20,46 +22,54 @@ except ImportError:
 # parameter for urlopen
 def reversepattern():
 	# store the response of URL
-	url = "https://datis.clowd.io/api/kvny"
+	url = "https://zdatis.clowd.io/api/kvny"
 	global information
 	global reversedpattern
-	response = urlopen(url)
-
-	# storing the JSON response
-	# from url in data
-	data_json = json.loads(response.read())
-
-	# print the json response
-	print(data_json)
-
-	airport = data_json[0]
-	datis = airport['datis']
-	# print(datis)
-
-	infostart: int = datis.find("INFO")
-	information = datis[infostart + 5]
-
-	runwaystart: int = datis.find('LNDG AND DEPG')
-	if runwaystart != -1:
-		runwayend: int = datis[runwaystart:].find('.')
-		landing = datis[runwaystart:runwaystart + runwayend]
-		if landing.find('34') > 0:
-			reversedpattern = True
-			return True
-		else:
-			reversedpattern = False
-			return False
+	try:
+		response = urlopen(url)
+	except URLError as e:
+		if hasattr(e, 'reason'):
+			print('We failed to reach a server.')
+			print('Reason: ', e.reason)
+		elif hasattr(e, 'code'):
+			print('The server couldn\'t fulfill the request.')
+			print('Error code: ', e.code)
 	else:
-		return False
+		# storing the JSON response
+		# from url in data
+		data_json = json.loads(response.read())
+
+		# print the json response
+		print(data_json)
+
+		airport = data_json[0]
+		datis = airport['datis']
+		# print(datis)
+
+		infostart: int = datis.find("INFO")
+		information = datis[infostart + 5]
+
+		runwaystart: int = datis.find('LNDG AND DEPG')
+		if runwaystart != -1:
+			runwayend: int = datis[runwaystart:].find('.')
+			landing = datis[runwaystart:runwaystart + runwayend]
+			if landing.find('34') > 0:
+				reversedpattern = True
+				return True
+			else:
+				reversedpattern = False
+				return False
+		else:
+			return False
 
 
 def run():
 	while True:
 		if reversepattern():
-			print("Reverse pattern...[", reversedpattern.__str__(), "]")
+			print("Reverse pattern...")
 			GPIO.output(8, GPIO.HIGH)
 		else:
-			print("Normal pattern...[", reversedpattern.__str__(), "]")
+			print("Normal pattern...")
 			GPIO.output(8, GPIO.LOW)
 		print("Information: ",information )
 		time.sleep(5)
@@ -75,16 +85,20 @@ def interrupthandler(signum, frame):
 	sys.exit()
 
 
-information = ""
-reversedpattern = False
-
-
-if __name__ == "__main__":
+def gpiosetup(pin):
 	GPIO.setwarnings(False)  # Ignore warning for now
 	GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 	GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 	GPIO.output(8, GPIO.HIGH)
 	time.sleep(0.25)
 	GPIO.output(8, GPIO.LOW)
+
+
+information = ""
+reversedpattern = False
+
+
+if __name__ == "__main__":
+	gpiosetup(8)
 	signal.signal(signal.SIGINT, interrupthandler)
 	run()
